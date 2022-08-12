df333c6860850844db724655bee145c35af3a02d
nitesh009 <niteshgnit3@gmail.com>
2022-08-12 11:44:42 +0530

    First Jenkins Project

---
+++ src/test/java/com/nkdebug/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.nkdebug.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	static Logger logger = LoggerFactory.getLogger(JenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Jenkins Test is running");
+	}
+
+}
